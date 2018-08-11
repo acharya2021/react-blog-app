9bcd0d151b31a9a1173ec2a5d29eafec695f7b7d
Abhisesh Acharya <aachary5@u.rochester.edu>
2018-08-11 13:46:59 +0545

    Set up a first route for PostsIndex

--- src/index.js
+++ src/index.js
@@ -1,15 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import {Provider} from 'react-redux';
+import {createStore, applyMiddleware} from 'redux';
+// the BrowserRouter object interacts with the history library
+// and decides what to do based on a change inside the url
+// the Route object provides configuration to react-router
+// in order to show a component based on the url
+import {BrowserRouter, Route} from 'react-router-dom';
 
-import App from './components/app';
 import reducers from './reducers';
+import PostsIndex from './components/posts_index';
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+    <Provider store={createStoreWithMiddleware(reducers)}>
+        <BrowserRouter>
+            {/*if the user goes to this route(path),I will show this component*/}
+            <div>
+                <Route path="/" component={PostsIndex}/>
+            </div>
+        </BrowserRouter>
+    </Provider>
+    , document.querySelector('.container'));
