2e4cb815749c8b752c56d0aa0b81acb67f54485f
Abhisesh Acharya <aachary5@u.rochester.edu>
2018-08-11 20:05:24 +0545

    Add conditional styling to form

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -3,19 +3,26 @@ import {Field, reduxForm} from 'redux-form';
 
 class PostsNew extends Component {
 
+
     renderField(field) {
+
+        const {meta: {touched, error}} = field;
+        const className = `form-group ${touched && error ? 'has-danger' : ""}`;
+
         // field.input is an object containing event handlers and props
         // ... communicates properties inside the field.input object
         // as props to the input tag
         return (
-            <div className="form-group">
+            <div className={className}>
                 <label>{field.label}</label>
                 <input
                     className="form-control"
                     type="text"
                     {...field.input}
                 />
-                {field.meta.touched ? field.meta.error : ""}
+                <div className="text-help">
+                    {touched ? error : ""}
+                </div>
             </div>
         );
     }
